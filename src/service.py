3df6944852fd69c2d2ba6d71dc3d922af190c037
root <root@ubuntu.(none)>
2013-05-05 19:05:21 -0300

    pedir productos desde la REST API

--- src/service.py
+++ src/service.py
@@ -5,22 +5,29 @@ from queryProcessing import ProcessingQuery
 from query import QueryFactory
 
 class RestApi:
-    print "RestApi startup"
-    def __init__(self, aQueryProcessor):
-        self.queryProcessor = aQueryProcessor
+	print "RestApi startup"
+	def __init__(self, aQueryProcessor, aValidProductsProvider):
+		self.queryProcessor = aQueryProcessor
+		self.validProductsProvider = aValidProductsProvider
 
-    def service(self):
-        return "PrecioJusto v0.0.0.0.0.0.0.0.0.0.0.5"
+	def service(self):
+		return "PrecioJusto v0.0.0.0.0.0.0.0.0.0.0.5"
 
-    def query(self, producto, rango=None):
-        cherrypy.response.headers["Content-Type"] =  "text/plain"
-        processingQuery = ProcessingQuery(producto)
-        processingQuery.setRango(rango)
+	def query(self, producto, rango=None):
+		cherrypy.response.headers["Content-Type"] =  "text/plain"
+		processingQuery = ProcessingQuery(producto)
+		processingQuery.setRango(rango)
 
-        self.queryProcessor.processQuery(processingQuery)
-        query = QueryFactory().createQuery(processingQuery)
-        return str(query)
+		self.queryProcessor.processQuery(processingQuery)
+		query = QueryFactory().createQuery(processingQuery)
+		return str(query)
+		
+	def products(self):
+		cherrypy.response.headers["Content-Type"] =  "text/plain"
+		return str(self.validProductsProvider.products())
 
 
-    service.exposed = True
-    query.exposed = True	
+	service.exposed = True
+	query.exposed = True
+	products.exposed = True
+
