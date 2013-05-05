5968bdaf6331d7f0cf1c26041b1ebe0801aab634
March <m2.march@gmail.com>
2013-05-05 19:24:34 -0300

    ValidProducts beautified.

--- src/validProducts.py
+++ src/validProducts.py
@@ -1,10 +1,10 @@
 from products import Product
 
 class ValidProductsProvider:
-	def products(self):
-		return [Product("Yerba"), Product("Azucar")]
+  def products(self):
+    return [Product("Yerba"), Product("Azucar")]
 
 
 if __name__ == "__main__":
-	productsList = ValidProductsProvider().products()
-	assert map(lambda p : p.name(), productsList) == ["YERBA", "AZUCAR"]
+  productsList = ValidProductsProvider().products()
+  assert map(lambda p : p.name(), productsList) == ["YERBA", "AZUCAR"]
