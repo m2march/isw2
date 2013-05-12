fe5c8d2179ff14b0298b83d3c1a64d0bdafef626
Ivan Postolski <ivan.postolski@mulesource.com>
2013-05-12 17:34:29 -0300

    improving interaction between frontend and backend. part 3

--- src/defaultModelManager.py
+++ src/defaultModelManager.py
@@ -1,14 +1,15 @@
 from defaultDataManager import *
 from defaultMemoryStore import *
 from offerFactory import *
+from modelManager import *
 
 class DefaultModelManager(ModelManager):
 	def __init__(self):				
 		memoryStore = DefaultMemoryStore()
 		dataManager = DefaultDataManager(memoryStore)
 		offerFactory = DefaultOfferFactory()
-		super(DefaultOfferFactory,self).__init__(self,dataManager,offerFactory,memoryStore)
-
-	def process(self, anInmmutableOfferQuery):
+		super(DefaultModelManager,self).__init__(dataManager,offerFactory,memoryStore)
 
 
+if __name__ == "__main__":
+	modelManager = DefaultModelManager()
--- src/modelManager.py
+++ src/modelManager.py
@@ -1,7 +1,16 @@
-class ModelManager(Object):
+class ModelManager(object):
 	def __init__(self,aDataManager, anOfferFactory, aStoreObject):
 		self.dataManager = aDataManager
-		self.offerFactory = offerFactory
+		self.offerFactory = anOfferFactory
 		self.storeObject = aStoreObject
-	def process(self, anInmmutableOfferQuery):
-		raise NotImplementedError("Should have implemented this")
+	def process(self, anOfferQuery):
+		#Get offers from outside
+		self.dataManager.getOffers()
+		#Parse and create offers
+		offers = self.offerFactory.createOffers(self.storeObject)
+		#filter by user requirements
+		aFilter = anOfferQuery.filter()		
+		offersFiltered = filter(lambda anOffer: aFilter.filter(anOffer), offers)
+		aStrategy = anOfferQuery.strategy()
+		offersFinished = aStrategy.prioritize(offersFiltered)
+		return offersFiltered
